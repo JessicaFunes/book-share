eafdbeb2aa99e952127c4eb8338b5c9d4419ba61
Michell Ayala <starships@outlook.com>
2016-04-02 23:56:32 -0600

    Search route. All shit works. Tests peding. Docs pending.

--- app.js
+++ app.js
@@ -1,21 +1,31 @@
 'use strict';
 
+const Http = require('http');
 const Path = require('path');
 const Express = require('express');
+const BodyParser = require('body-parser');
+const Logger = require('morgan');
+const Qs = require('qs');
 
 const publicPath = Path.join(__dirname, 'app/public');
 const viewsPath = Path.join(__dirname, 'app/views');
 
 const app = Express();
 
+app.use(Logger('tiny'));
+app.use(Express.static(publicPath));
+app.use(BodyParser.json());
+app.use(BodyParser.urlencoded({ extended: false }));
+
 app.set('view engine', 'jade');
 app.set('views', viewsPath);
-app.use(Express.static(publicPath));
 
-app.get('/', function (req, res) {
-  res.render('home', {
-    name: 'hola mundo'
-  });
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.get('/search', (req, res) => {
+  res.render('search', { q: Qs.stringify(req.query) });
 });
 
 module.exports = app;
