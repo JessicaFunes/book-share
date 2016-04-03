68c5b09ce888d97927dcfeb579b316ae876561a0
Michell Ayala <starships@outlook.com>
2016-04-02 21:50:08 -0600

    Jade added. Template engine up and running.

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 'use strict';
 
+const Path = require('path');
 const Express = require('express');
 
+const publicPath = Path.join(__dirname, 'app/public');
+const viewsPath = Path.join(__dirname, 'app/views');
+
 const app = Express();
+
+app.set('view engine', 'jade');
+app.set('views', viewsPath);
+app.use(Express.static(publicPath));
+
+app.get('/', function (req, res) {
+  res.render('home', {
+    name: 'hola mundo'
+  });
+});
 
 module.exports = app;
